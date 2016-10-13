867def559430e3071f27b1844022f0d722600803
falconetpt <ipsiart@gmail.com>
2016-10-13 13:39:12 +0000

    triangle bash script added

---
+++ triangle.sh
@@ -0,0 +1,13 @@
+echo "Lado 1"
+read a
+echo "Lado 2"
+read b
+echo "Lado 3"
+read c
+
+if [ $(($b + $a)) -gt $c ] && [ $(($c + $a)) -gt $b ] && [ $(($b + $c)) -gt $a ]
+then
+    
+else
+	echo "Triangulo Impossivel"
+fi
